169b06eea418ce021d30db6b69285c514af094da
Jon Isaacson <jon.w.isaacson@gmail.com>
2020-11-04 23:06:58 -0800

    Added new summary statistics and boxplot

--- Practice.R
+++ Practice.R
@@ -6,3 +6,7 @@ x2<-rnorm(1000, 3, 4)
 plot(density(x1), main="X1 vs X2", lwd=2)
 lines(density(x2), col="red", lwd=2)
 
+mean(x1)
+mean(x2)
+t.test(x1, x2)
+boxplot(x1, x2, xlab = "Group", ylab="Value")
